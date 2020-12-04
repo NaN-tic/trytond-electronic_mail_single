4dd10cf9fac24209540527d8dcd766d30e6b7b26
Albert Cervera i Areny <albert@nan-tic.com>
2020-12-04 13:02:03 +0100

    Pyflakes fixes.

--- __init__.py
+++ __init__.py
@@ -2,14 +2,14 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .template import *
-from .electronic_mail_wizard import *
+from . import template
+from . import electronic_mail_wizard
 
 
 def register():
     Pool.register(
-        Template,
+        template.Template,
         module='electronic_mail_single', type_='model')
     Pool.register(
-        GenerateTemplateEmail,
+        electronic_mail_wizard.GenerateTemplateEmail,
         module='electronic_mail_single', type_='wizard')
